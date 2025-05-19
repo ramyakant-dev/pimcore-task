89e82ed56bc60f0d79ecf76652f5bc5dad4aeb4c
Ramyakant <ramyakant.samal@gmail.com>
2025-05-19 11:33:25 +0200

    Initial setup and product class

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\UuidBundle\PimcoreUuidBundle::class => ['all' => true],
+    Pimcore\Bundle\GenericExecutionEngineBundle\PimcoreGenericExecutionEngineBundle::class => ['all' => true],
+];
